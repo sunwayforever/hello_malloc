84fd1894568f99ad76a9d1fb97c3b9048d45bfac
sunway <wei.sun@hexintek.com>
2022-08-18 15:19:14 +0800

    nil

--- malloc.h
+++ malloc.h
@@ -1,16 +1,16 @@
-// 2022-08-18 14:24
 #ifndef MALLOC_H
 #define MALLOC_H
-
 #include "mspace_malloc.h"
 
 void* default_mspace;
-
-inline void init_default_mspace(void* base, size_t capacity) {
-    default_mspace = create_mspace_with_base(base, 10240, 0);
+static inline void init_default_mspace(void* base, size_t capacity) {
+    default_mspace = create_mspace_with_base(base, capacity, 0);
 }
 
-void* malloc(size_t n) { return mspace_malloc(default_mspace, n); }
-void free(void* mem) { mspace_free(default_mspace, mem); }
+static inline void* malloc(size_t n) {
+    return mspace_malloc(default_mspace, n);
+}
+
+static inline void free(void* mem) { mspace_free(default_mspace, mem); }
 
 #endif  // MALLOC_H
--- test.c
+++ test.c
@@ -1,14 +1,24 @@
-// 2022-08-18 10:26
 #include <stdio.h>
 
 #include "malloc.h"
 
 char buffer[10240];
+char buffer2[10240];
+
 int main(int argc, char *argv[]) {
+    /* NOTE: case 1, wrap malloc with default_mspace  */
     init_default_mspace(buffer, sizeof(buffer));
     for (int i = 0; i < 10; i++) {
-        void *x = malloc(10);
+        void *x = malloc(32);
         printf("%p\n", x);
-        free(x);
+    }
+    printf("\n");
+
+    /* NOTE: case 2, use mspace directly */
+    mspace space = create_mspace_with_base(buffer2, sizeof(buffer2), 0);
+    for (int i = 0; i < 10; i++) {
+        void *x = mspace_malloc(space, 32);
+        mspace_free(space, x);
+        printf("%p\n", x);
     }
 }

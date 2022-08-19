e29cdb509a4566c9370b6d666306b9e4fe02cabb
sunway <wei.sun@hexintek.com>
2022-08-19 19:26:56 +0800

    nil

--- mspace/mspace_malloc.h
+++ mspace/mspace_malloc.h
@@ -2,15 +2,40 @@
 #define MALLOC_H
 #include "dlmalloc.h"
 
+#ifndef USE_HXD_PREFIX
+#define hxd_malloc malloc
+#define hxd_free free
+#endif
+
+#ifdef DDEBUG_HIST
+#include <stdio.h>
+#endif
+
 static void* default_mspace;
 static inline void init_default_mspace(void* base, size_t capacity) {
     default_mspace = create_mspace_with_base(base, capacity, 0);
 }
 
-static inline void* malloc(size_t n) {
+static inline void* hxd_malloc(size_t n) {
+#ifdef DEBUG_HIST
+    printf("HIST:ALLOC:%ld\n", n);
+    size_t* ret = (size_t*)mspace_malloc(default_mspace, n + sizeof(size_t));
+    *ret = n;
+    return (void*)(ret + 1);
+#else
     return mspace_malloc(default_mspace, n);
+#endif
 }
 
-static inline void free(void* mem) { mspace_free(default_mspace, mem); }
+static inline void hxd_free(void* mem) {
+#ifdef DEBUG_HIST
+    size_t* orig = (size_t*)mem - 1;
+    size_t n = *orig;
+    printf("HIST:DEALLOC:%ld\n", n);
+    mspace_free(default_mspace, (void*)orig);
+#else
+    mspace_free(default_mspace, mem);
+#endif
+}
 
 #endif  // MALLOC_H
--- mspace/test_mspace.c
+++ mspace/test_mspace.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "mspace_malloc.h"
 
@@ -8,18 +9,10 @@ char buffer2[10240];
 int main(int argc, char *argv[]) {
     /* NOTE: case 1, wrap malloc with default_mspace  */
     init_default_mspace(buffer, sizeof(buffer));
-    for (int i = 0; i < 10; i++) {
-        void *x = malloc(32);
+    for (int i = 0; i < 100; i++) {
+        void *x = hxd_malloc(i);
+        memset(x, 1, i);
         printf("%p\n", x);
+        hxd_free(x);
     }
-    printf("\n");
-
-    /* NOTE: case 2, use mspace directly */
-    mspace space = create_mspace_with_base(buffer2, sizeof(buffer2), 0);
-    for (int i = 0; i < 10; i++) {
-        void *x = mspace_malloc(space, 32);
-        mspace_free(space, x);
-        printf("%p\n", x);
-    }
-    destroy_mspace(space);
 }

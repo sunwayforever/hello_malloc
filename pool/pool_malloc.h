a6103d89506ebea49786cfe1282df434d04f8ad8
sunway <wei.sun@hexintek.com>
2022-08-19 14:21:08 +0800

    nil

--- pool/pool_malloc.h
+++ pool/pool_malloc.h
@@ -1,6 +1,6 @@
 // 2022-08-19 11:02
-#ifndef BUMP_POINTER_MALLOC_H
-#define BUMP_POINTER_MALLOC_H
+#ifndef POOL_MALLOC_H
+#define POOL_MALLOC_H
 #include <stddef.h>
 struct Chunk {
     struct Chunk* next;
@@ -13,4 +13,4 @@ typedef struct {
 Pool* init_pool(void* base, size_t capacity, size_t size, size_t count);
 void* pool_malloc(Pool* pool);
 void pool_free(Pool* pool, void* mem);
-#endif  // BUMP_POINTER_MALLOC_H
+#endif  // POOL_MALLOC_H
--- pool/test_pool.c
+++ pool/test_pool.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "pool_malloc.h"
 
+//  NOTE: internally `Pool` resides at the beginning of `buffer`
 char buffer[32 * 10 + sizeof(Pool)];
 
 int main(int argc, char *argv[]) {
+    /* NOTE: due to data alignment, runtime error will be thrown if buffer is
+     * not large enough to hold required objects */
     Pool *pool = init_pool(buffer, sizeof(buffer), 32, 10);
     for (int i = 0; i < 11; i++) {
         void *x = pool_malloc(pool);
+        if (x != NULL) {
+            memset(x, 1, 32);
+        }
         printf("%p\n", x);
     }
 
     pool = init_pool(buffer, sizeof(buffer), 32, 10);
     for (int i = 0; i < 10; i++) {
         void *x = pool_malloc(pool);
+        if (x != NULL) {
+            memset(x, 1, 32);
+        }
         printf("%p\n", x);
         pool_free(pool, x);
     }
